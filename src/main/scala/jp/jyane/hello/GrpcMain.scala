9102a59a7d2368f25763caa7406008ad70c9450c
jyane <jyane.kstm@gmail.com>
2017-08-25 22:44:35 +0900

    fix

--- src/main/scala/jp/jyane/hello/GrpcMain.scala
+++ src/main/scala/jp/jyane/hello/GrpcMain.scala
@@ -1,22 +1,31 @@
 package jp.jyane.hello
 
 import io.grpc.ServerBuilder
+import io.grpc.health.v1.HealthCheckResponse.ServingStatus
 import io.grpc.protobuf.services.ProtoReflectionService
+import io.grpc.services.HealthStatusManager
 import jp.jyane.hello.proto.GreeterGrpc
 import jp.jyane.hello.service.GreeterService
 
 import scala.concurrent.ExecutionContext
 
 object GrpcMain {
+  private[this] val healthStatusManager = new HealthStatusManager
+
+  healthStatusManager.setStatus("Greeter", ServingStatus.NOT_SERVING)
+  healthStatusManager.setStatus("", ServingStatus.SERVING)
+
   def main(args: Array[String]): Unit = {
     val server = ServerBuilder
       .forPort(11111)
-      .addService(ProtoReflectionService.newInstance())
+      .addService(healthStatusManager.getHealthService)
       .addService(GreeterGrpc.bindService(new GreeterService, ExecutionContext.global))
+      .addService(ProtoReflectionService.newInstance())
       .build()
 
     server.start()
     server.awaitTermination()
+
 
     sys.addShutdownHook {
       server.shutdown()
